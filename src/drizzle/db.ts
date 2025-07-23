aae3338b96884ea31cb9cd05f31d02b5dbc25bf9
Gakenye8741 <brayogakenye04@gmail.com>
2025-07-23 12:31:44 +0300

    Configured some changes iorder to allow the userto do transaction

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -1,13 +1,19 @@
 import "dotenv/config";
-import { neon } from "@neondatabase/serverless"
-import { drizzle } from "drizzle-orm/neon-http"
-import * as schema from  "./schema";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
+import * as schema from "./schema";
 
 if (!process.env.DATABASE_URL) {
   throw new Error("DATABASE_URL is not defined in environment variables");
 }
 
-const client = neon(process.env.DATABASE_URL);
-const db = drizzle(client, { schema, logger: true });
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // if needed by Neon, often required
+  },
+});
+
+const db = drizzle(pool, { schema, logger: true });
 
 export default db;
